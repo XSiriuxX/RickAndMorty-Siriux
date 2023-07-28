6934c18d6379300d467ac56bcaaac7ea42947c7e
GCespedes <giovannice22@hotmail.com>
2023-07-28 09:01:09 -0500

    m4

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,8 @@
 const PORT = 3001;
 const server = require("./app");
+const { conn } = require("./DB_connection");
 
 server.listen(PORT, () => {
+  conn.sync({ force: true });
   console.log("Server raised at port: " + PORT);
 });
--- Server/src/utils/users.js
+++
@@ -1,1 +0,0 @@
-module.exports = [{ email: "giovannice22@hotmail.com", password: "A123" }];
